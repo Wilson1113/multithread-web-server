a85d2ed0e27d85e91a0c72833d15fb34bdcb80c1
Wilson1113 <wilsonteoshiyang@gmail.com>
2026-09-09 20:52:30 +0800

    implement multithread with inifnite number of thread - for each reqeust come in spawn a thread to handle it

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@ use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        handle_connnection(stream);
+        thread::spawn(|| {
+            handle_connnection(stream);
+        });
     }
 }
 
@@ -17,10 +20,13 @@ fn handle_connnection(mut stream: TcpStream) -> () {
     let reader = BufReader::new(&stream);
 
     let status_line = reader.lines().next().unwrap().unwrap();
-    let (status_line, file_name) = if status_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    let (status_line, file_name) = match &status_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
     let contents = fs::read_to_string(file_name).unwrap();
