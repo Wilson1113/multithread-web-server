194d107f1dce5c1640ed89bcb8de054a3536bfe0
Wilson1113 <wilsonteoshiyang@gmail.com>
2026-09-09 12:36:47 +0800

    Update the request, response handle: - add if selection to distinguish request status line - response 200 OK/ 404 NOT FOUND based on condition

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 A practise project of using Rust to learn about web server and thread pool
 
 This project is mainly use for practising so no advanged crate used, and no ai used to build anything, if you are interested to take practise also can refer to this https://doc.rust-lang.org/book/ch21-01-single-threaded.html to build own version
+
+# Phrase 1
+Practising simulate the single thread webserver with simple TcpListener and response handler to send back the response which establish simple request and response connection.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -14,12 +15,18 @@ fn main() {
 
 fn handle_connnection(mut stream: TcpStream) -> () {
     let reader = BufReader::new(&stream);
-    let http_request: Vec<_> = reader
-    .lines()
-    .map(|result| result.unwrap())
-    .take_while(|line| !line.is_empty())
-    .collect();
 
-    let response = "HTTP/1.1 200 OK \r\n\r\n";
+    let status_line = reader.lines().next().unwrap().unwrap();
+    let (status_line, file_name) = if status_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(file_name).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
+    
 }
