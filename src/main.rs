cc70be1ba97defabfbbb3613f7cd30020f0a86c1
Wilson1113 <wilsonteoshiyang@gmail.com>
2026-09-11 00:12:45 +0800

    Refactor ThreadPool to use Option for sender and improve worker shutdown handling

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
+use multithread_web_server::ThreadPool;
 use std::{
     fs,
-    io::{prelude::*, BufReader},
+    io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
     thread,
     time::Duration,
 };
-use multithread_web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
+
         pool.execute(|| {
-            handle_connnection(stream);
+            handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
-fn handle_connnection(mut stream: TcpStream) -> () {
-    let reader = BufReader::new(&stream);
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let status_line = reader.lines().next().unwrap().unwrap();
-    let (status_line, file_name) = match &status_line[..] {
+    let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
             thread::sleep(Duration::from_secs(5));
@@ -32,10 +35,11 @@ fn handle_connnection(mut stream: TcpStream) -> () {
         _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
-    let contents = fs::read_to_string(file_name).unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
     stream.write_all(response.as_bytes()).unwrap();
-    
 }
